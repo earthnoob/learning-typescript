433e0f8d4a124c25719de9393587c15b7a2ed4df
earthnoob <lammap98@gmail.com>
2021-03-05 17:14:39 +0700

    Object Types

--- app.ts
+++ app.ts
@@ -1,20 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('Incorrect input!');
-  // }
-  const result = n1 + n2;
+const person: {
+  name: string,
+  age: number,
+} = {
+  name: 'Johnathan',
+  age: 20,
+};
 
-  if (showResult) {
-    console.log(phrase + result);
-  }
-  return result;
-}
-
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-const result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+console.log(person.name);
