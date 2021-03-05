3f3c386392bdfa04ead80dac1b0d00189cde67b6
earthnoob <lammap98@gmail.com>
2021-03-05 17:00:55 +0700

    Type Assignment & Type Inference

--- app.ts
+++ app.ts
@@ -10,7 +10,8 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   return result;
 }
 
-const number1 = 5;
+let number1: number;
+number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = 'Result is: ';
