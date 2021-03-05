e944b93939288eef7fc2cf4989ccfe0838dd1b2d
earthnoob <lammap98@gmail.com>
2021-03-05 17:52:32 +0700

    Working with Tuples

--- app.ts
+++ app.ts
@@ -2,11 +2,20 @@ const person: {
   name: string,
   age: number,
   hobbies: string[],
+  role: [number, string],
 } = {
   name: 'Johnathan',
   age: 20,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+// Legal
+// person.role.push('admin');
+
+// Illegal
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
