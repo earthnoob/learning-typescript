a59789b307a1d63ef3852df07c474a1a74d87b0c
earthnoob <lammap98@gmail.com>
2022-01-22 18:09:01 +0700

    private and public Access Modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,10 +9,28 @@ class Department {
   describe(this: Department) {
     console.log('Department:', this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 console.log(accounting);
 
-const accountingCopy = { name: 'Dummy', describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: 'Dummy', describe: accounting.describe };
+// accountingCopy.describe();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
+
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
